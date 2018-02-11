a413fd8ce912ebc1fcc5feb2dc664948631cc02e
Kalpana Madhalam <mkalpanais@gmail.com>
2018-02-11 17:30:54 +1100

    Added enzyme for unit tests.

--- src/components/MediaCard/MediaCard.test.js
+++ src/components/MediaCard/MediaCard.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import MediaCard from './MediaCard';
+import tile from '../../img/tile.jpg';
+import logo from '../../img/logo.png';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<MediaCard />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it('renders without crashing.', () => {
+  shallow(<MediaCard />);
+});
+
+it('should match snapshot..', () => {
+  const wrapper = shallow(<MediaCard
+    image={tile}
+    imageAltText="Home and Away"
+    logo={logo}
+    logoAltText="Seven"
+    description="Home and Away"
+  />);
+  expect(wrapper).toMatchSnapshot();
 });
