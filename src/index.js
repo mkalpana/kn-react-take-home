4d63a12affe5d535d001de06cca4865f10b33cbe
Kalpana Madhalam <mkalpanais@gmail.com>
2018-02-11 16:04:35 +1100

    Added global styles via styled components.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import { injectGlobal } from 'styled-components';
+
+injectGlobal`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: sans-serif;
+    font-size: 16px;
+    background-color: #0E0E0E;
+  }
+`;
 
 ReactDOM.render(<App />, document.getElementById('root'));
